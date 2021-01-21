156280c369bc2d73e3a92375608fd669259223f5
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-20 16:13:58 -0800

    split inputted sentence into an array and declare our vowels
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,11 @@ $(document).ready(function() {
     $("#result").hide();
 
     const sentence = $("input#sentence").val();
+    const splitSent = sentence.split("");
+    console.log(splitSent)
+
+    const vowels = ["a","e","i","o","u"]
+    let encryption = [];
     console.log(sentence)
   })
 })
