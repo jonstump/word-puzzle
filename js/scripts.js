b089bc16d3693db5139683f52d6faa931bd9f829
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-20 16:30:52 -0800

    add if statement to evaluate elements for letter type. WIP, not final code
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,19 @@ $(document).ready(function() {
     $("#result").hide();
 
     const sentence = $("input#sentence").val();
-    const splitSent = sentence.split("");
-    console.log(splitSent)
+    const splitArray = sentence.split("");
+    //console.log(splitArray)
 
     const vowels = ["a","e","i","o","u"]
     let encryption = [];
-    console.log(sentence)
+    //console.log(sentence)
+
+    for(let i = 0; i < splitArray.length; i++) {
+      if (vowels === i) {
+        i = "-";
+        encryption.push(splitArray)
+      }
+      
+    }
   })
 })
